9582c206d7355e8eda40799d10beda16caf73b4c
Allan Heremi <allanheremidev@gmail.com>
2023-10-28 11:47:24 +0200

    Feat: game 1-5 won;

--- test/game3Test.js
+++ test/game3Test.js
@@ -22,12 +22,21 @@ describe('Game3', function () {
     const { game, signer } = await loadFixture(deployContractAndSetVariables);
 
     // you'll need to update the `balances` mapping to win this stage
+    const signer1 = ethers.provider.getSigner(0);
+    const signer2 = ethers.provider.getSigner(1);
+    const signer3 = ethers.provider.getSigner(2);
 
     // to call a contract as a signer you can use contract.connect
-    await game.connect(signer).buy({ value: '1' });
+    await game.connect(signer1).buy({ value: '5' });
+    await game.connect(signer2).buy({ value: '10' });
+    await game.connect(signer3).buy({ value: '3' });
+
+    const address1 = await signer1.getAddress();
+    const address2 = await signer2.getAddress();
+    const address3 = await signer3.getAddress();
 
     // TODO: win expects three arguments
-    await game.win();
+    await game.win(address1, address2, address3);
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
--- test/game5Test.js
+++ test/game5Test.js
@@ -1,19 +1,40 @@
 const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 const { assert } = require('chai');
+const { ethers } = require('hardhat');
 
 describe('Game5', function () {
   async function deployContractAndSetVariables() {
     const Game = await ethers.getContractFactory('Game5');
     const game = await Game.deploy();
-
-    return { game };
-  }
-  it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+    const signer = await ethers.provider.getSigner(0);
 
     // good luck
 
-    await game.win();
+    const threshold = 0x00ffffffffffffffffffffffffffffffffffffff;
+    let valid;
+    let address;
+    let wallet;
+    while (!valid) {
+      wallet = ethers.Wallet.createRandom();
+      address = await wallet.getAddress();
+
+      if (address < threshold) {
+        valid = true;
+        wallet = wallet.connect(ethers.provider);
+      }
+    }
+
+    await signer.sendTransaction({
+      to: address,
+      value: ethers.utils.parseEther('0.1'),
+    });
+
+    return { game, wallet };
+  }
+  it('should be a winner', async function () {
+    const { game, wallet } = await loadFixture(deployContractAndSetVariables);
+
+    await game.connect(wallet).win();
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
